e371c14d946eb2ec9cc1efb073caa361b3d2c164
Yuancheng Zhang <en@endaye.com>
2019-12-07 23:39:21 +0800

    finish tutorial and out-of-date shit

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ console.log(stripeSecretKey, stripePublicKey)
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const stripe = require('stripe')(stripeSecretKey)
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/store', (req, res) => {
@@ -24,6 +26,37 @@ app.get('/store', (req, res) => {
             res.render('store.ejs', {
                 stripePublicKey: stripePublicKey,
                 items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+app.post('/purchase', (req, res) => {
+    fs.readFile('items.json', (error, data) => {
+        if (error) {
+            res.status(500).end()
+        } else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            req.body.items.forEach((item) => {
+                const itemJson = itemsArray.find((i) => {
+                    return i.id == item.id
+                })
+                total = total + itemJson.price * item.quantity
+            })
+            stripe.charges.create({
+                amount: total,
+                source: req.body.stripeTokenId,
+                currency: 'usd'
+            }).then(() => {
+                console.log('Charge Successful')
+                res.json({
+                    message: 'Successfully purchased items'
+                })
+            }).catch(() => {
+                console.log('Charge Fail')
+                res.status(500).end()
             })
         }
     })
