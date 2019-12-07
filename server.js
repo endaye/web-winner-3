a700b939091037c38a4c5184c3054a504f59c1e8
Yuancheng Zhang <en@endaye.com>
2019-12-07 15:12:34 +0800

    add stripe keys

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // ref: https://www.youtube.com/watch?v=mI_-1tbIXQI
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublicKey)
+
 const express = require('express')
 const app = express()
 
